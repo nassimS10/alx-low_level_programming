72a146793f10d5935d1ab240ebb608ced380b581
nassimS10 <sabornassim5@gmail.com>
2023-03-16 14:45:53 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/*
+ * main - this is a main function
+ *
+ * Return: result 
+ */
+
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle \n");
-return(0);
+        puts("\"Programming is like building a multilingual puzzle");
+        return(0);
 }
 
