13e74ca18a7280a9b610e389898002707e961eaf
nassimS10 <sabornassim5@gmail.com>
2023-03-27 18:40:13 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - a function that prints a string
+ * @str: string input
+ * Return: string
+ */
+void _puts(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
