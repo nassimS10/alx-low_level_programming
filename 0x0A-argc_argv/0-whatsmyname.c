c5e2098c4e1062a96c5e77af901a5ecfcc8061fc
nassimS10 <sabornassim5@gmail.com>
2023-04-10 15:17:06 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -  print the name
+ * @argc: arguement c
+ * @argv: arr arguement strings
+ * Return: 0
+ **/
+int main(int argc, char *argv[])
+{
+(void)argc;
+printf("%s\n", *argv[0]);
+return (0);
+}
