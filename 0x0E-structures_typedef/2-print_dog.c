0097e73035a4619c396a91f2e71622b8a31ea8cc
nassimS10 <sabornassim5@gmail.com>
2023-04-17 19:31:11 -0700

    task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,21 +1,21 @@
-#include "dog.h"
+
 #include <stdio.h>
 #include <stdlib.h>
-/**
- * init_dog - init a var struct dog.
- * @d: pointer
- * @name: pointer
- * @age: age
- * @owner: pointer
- * Return: return
- */
+#include "dog.h"
 
-void init_dog(struct dog *d, char *name, float age, char *owner)
+/**
+ * print_dog - fanc
+ * @d: struct d
+ */
+void print_dog(struct dog *d)
 {
-if (d)
-{
-printf("Name: %s\n", d->name ? d->name : "(nil)");
-printf("Age: %f\n", d->age);
-printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
-}
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
