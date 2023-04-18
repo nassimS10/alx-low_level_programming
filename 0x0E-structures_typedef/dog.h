e5db2015fd5107e51bd2b1badcb4f7d29c3390f2
nassimS10 <sabornassim5@gmail.com>
2023-04-17 18:47:33 -0700

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,9 +9,19 @@
 */
 struct dog
 {
-char *name;
-float age;
-char *owner
+	char *name;
+	float age;
+	char *owner;
 };
 
-#endif
+typedef struct dog dog_h;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+#endiff
