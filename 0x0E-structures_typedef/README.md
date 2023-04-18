c9c7f97e9dfaa44dd2644870784af25fa1ce8582
nassimS10 <sabornassim5@gmail.com>
2023-04-17 18:37:20 -0700

    task 0

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+this is readme file for 0x0E-structures_typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+/**
+*struct dog - a struct
+*@name: name
+*@age: age
+*@owner: owner
+*description: dog struct
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner
+};
+
+#endif
