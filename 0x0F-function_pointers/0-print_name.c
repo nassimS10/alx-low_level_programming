7fa86f819f1df208aa9efa51bb69d7b0aebd6df2
nassimS10 <sabornassim5@gmail.com>
2023-04-19 17:48:17 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - print name
+ * @name: char
+ * @f: pointer
+ **/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+this a readme file for 0x0F-function_pointers
