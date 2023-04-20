b4489b32e2a369f8d2255ecad001b2185e7ce84a
nassimS10 <sabornassim5@gmail.com>
2023-04-19 17:50:46 -0700

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,10 +4,11 @@
  * print_name - print name
  * @name: char
  * @f: pointer
+ * Return: return
  **/
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
-
+	return ();
 	f(name);
 }
