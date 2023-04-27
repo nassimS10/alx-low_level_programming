5b33b2921247ec5c29957e457ecccf02ee9761ea
nassimS10 <sabornassim5@gmail.com>
2023-04-27 15:59:02 -0700

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - print list
+ * @h: pointer h
+ *
+ * Return: raturn
+ */
+size_t print_list(const list_t *h)
+{
+size_t l = 0;
+
+while (h)
+{
+if (!h->str)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", h->len, h->str);
+h = h->next;
+l++;
+}
+return (l);
+}
