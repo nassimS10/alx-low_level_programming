673f4377054815f2a07b7cf4b5107a17af548f35
nassimS10 <sabornassim5@gmail.com>
2023-05-01 13:44:21 -0700

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib>
+/**
+ * free_listint2 - Free listint2.
+ * @head: ptr of param head
+ *
+ * Return : empty without return
+ */
+
+void free_listint2(listint_t **head)
+{
+listint_t *still;
+
+if (!head)
+return;
+
+while (*head != NULL)
+{
+still = (*head)->next;
+free(*head);
+*head = still;
+}
+head = NULL;
+}
