19d9e14a225464be5749954682e4c0cdde28ce41
nassimS10 <sabornassim5@gmail.com>
2023-05-01 15:00:27 -0700

    task 11

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * reverse_listint - reverse list
+ * @head: ptr head first node param
+ *
+ * Return: return
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *next, *prev;
+*head = h
+*perv = p
+p = NULL;
+w
