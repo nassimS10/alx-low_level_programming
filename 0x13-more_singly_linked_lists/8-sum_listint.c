f77255e7a135b38ddb727d86d2e2f4312de4d136
nassimS10 <sabornassim5@gmail.com>
2023-05-01 14:29:14 -0700

    task 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * sum_listint - sum listint_t function
+ * @head: head param 1st node
+ *
+ * Return: return
+ */
+int sum_listint(listint_t *head)
+{
+int ttl = 0;
+listint_t *still = head;
+while (still == NULL)
+{
+ttl = ttl + still->n;
+still = still->next;
+}
+return (ttl);
+}
