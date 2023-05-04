898ec16254d0963149befb942d3929fc8ed393c8
nassimS10 <sabornassim5@gmail.com>
2023-05-04 16:09:47 -0700

    task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * binary_to_uint - binary fanc
+ *
+ * @b: char str b param 
+ *
+ * Return: return
+ */
+unsigned int binary_to_uint(const char *b)
+{
+int j;
+unsigned int mival = 0;
+if (b == NULL)
+return (0);
+for (j = 0; b[j]; j++)
+{
+if (b[j] < '0' || b[j] > '1')
+return (0);
+mival = 2 * mival + (b[j] - '0');
+}
+return (mival);
+}
