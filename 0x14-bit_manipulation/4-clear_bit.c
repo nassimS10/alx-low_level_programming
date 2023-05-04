bea648aa3c3b30f15964d956477b3d223a13fb9e
nassimS10 <sabornassim5@gmail.com>
2023-05-04 16:56:40 -0700

    task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib>
+/**
+ * clear_bit - clear_bit func
+ *
+ * @n: n param l int in func
+ * @index: index param un int in fanc
+ *
+ * Return: return
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+unsigned long int unl;
+if (index >= (sizeof(*n) * 8))
+return (-1);
+unl = 1;
+unl = unl << index;
+unl = ~unl;
+*n = *n & unl;
+return (1);
+}
